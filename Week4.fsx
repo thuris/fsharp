33ac073729158cdb48ef5c4baa37047c5ba19001
Shawn Thuris <shawn@thur.is>
2017-12-09 14:02:09 -0800

    Added wk 4 script

--- Week4.fsx
+++ Week4.fsx
@@ -1,3 +1,18 @@
+// document markup language
 
-let nucleotides = 'ACCTGGTGTG'
-let mappy = 
+type DocumentBlock =
+    |Paragraph of string
+    |Title of string
+
+let formatAsHtml (text:DocumentBlock) = 
+    match text with
+    |Paragraph(text) -> "<p>" + text + "</p>" 
+    |Title(text) -> "<h1>" + text + "</h1>"
+
+let testText = Paragraph("Hello")
+printfn "%s" (formatAsHtml testText)
+
+let formatAsMkdn (text:DocumentBlock) = 
+    match text with
+    |Paragraph(text) -> text + "  " 
+    |Title(text) -> "#" + text
