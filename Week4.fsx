5fc4dadcf3b9002fc8382a97738bb2fc27509e93
Shawn Thuris <shawn@thur.is>
2017-12-10 14:37:12 -0800

    Incorporating local changes

--- Week4.fsx
+++ Week4.fsx
@@ -1,3 +1,23 @@
+open System.Text.RegularExpressions
+open System.Runtime.InteropServices
+open System.Runtime.Remoting.Messaging
 
-let nucleotides = 'ACCTGGTGTG'
-let mappy = 
+let nTides = "ACCTGGTGTG"
+
+//first using fold
+let foldEm = for i in ['A';'C';'G';'T'] do
+    Seq.fold iterate (fun x -> x = i) nTides -> results
+    printf "%i" results
+
+//next iterating with match
+let matchEm = for i it nTides do
+    match i with
+    |'A'
+    |'C'
+    |'G'
+    |'T'
+
+//finally recursion
+let rec recursEm input = 
+    if input = 0 then input
+    else input + recursEm (input - 1)
